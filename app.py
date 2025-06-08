b9d830dc9fb800656e3307807bfbe857c30a02c0
Samuel-Tomas <samo.tomas@centrum.sk>
2025-06-08 13:15:09 +0200

    ovládanie led, odosielanie hodnot
    Pridané ovládanie jasu LED pomocou slidera na stránke. Odosielanie hodnôt cez sériový port.

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 import serial
 import threading
 import time
 
 app = Flask(__name__)
-
-# Inicializácia sériového portu – uprav podľa svojho portu
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-# Zoznam na uchovávanie údajov
 light_data = []
 
-# Funkcia na čítanie údajov zo sériového portu
 def read_from_serial():
     global light_data
     while True:
@@ -22,24 +18,28 @@ def read_from_serial():
                     value = int(line[2:])
                     timestamp = int(time.time())
                     light_data.append((timestamp, value))
-                    # ponecháme len posledných 100 hodnôt
                     if len(light_data) > 100:
                         light_data = light_data[-100:]
                 except:
                     pass
 
-# Spustenie čítacieho vlákna
 threading.Thread(target=read_from_serial, daemon=True).start()
 
-# Hlavná stránka – zobrazuje základné HTML
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# API endpoint na získanie údajov vo formáte JSON
 @app.route('/light_data')
 def get_light_data():
     return jsonify(light_data)
 
+@app.route('/set_brightness', methods=['POST'])
+def set_brightness():
+    percent = int(request.form['percent'])  # percento z formulára
+    brightness = int(percent * 2.55)        # prevod na rozsah 0–255
+    ser.write(f"{brightness}\n".encode())   # odoslanie do Arduina
+    return '', 204
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
